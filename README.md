5bb2b891ec93e0e33ecb2d70cf51c4b2a651ac6f
Pavel Dobranov <pdobranov@codixfr.private>
2019-02-13 22:22:37 +0200

    publish for demo

---
+++ README.md
@@ -0,0 +1,41 @@
+# OBS2GO App Place
+
+## Requirements
+
+- [Node.js](https://nodejs.org/en/ "Node.js")
+
+## Setup
+
+### 1. Navigate to the project folder
+
+```shell
+$ cd obs2go-appplace
+```
+
+### 2. Install dependencies
+
+Install project dependencies and developer tools listed in package.json by running:
+
+```shell
+$ npm install
+```
+
+### 3. Start developing
+
+When it's done installing, you can start developing by running:
+
+```shell
+$ npm run dev
+```
+
+This command will starts a local web server that watches the files in `obs2go-appplace/src/` for changes.
+
+### 4. Build
+
+Create a production-ready build by running:
+
+```shell
+$ npm run build
+```
+
+After running this command, the `obs2go-appplace/dist/` folder will contain the production-ready build.
---
+++ gulpfile.js
@@ -0,0 +1,74 @@
+const gulp = require('gulp');
+const sass = require('gulp-sass');
+const purgeCss = require('gulp-purgecss');
+const cleanCss = require('gulp-clean-css');
+const browserSync = require('browser-sync');
+const del = require('del');
+
+const paths = {
+  html: {
+    src: './src/index.html',
+    dest: './dist'
+  },
+  styles: {
+    src: './src/styles/styles.scss',
+    dest: './dist/styles'
+  },
+  images: {
+    src: './src/images/*',
+    dest: './dist/images'
+  }
+};
+
+const server = browserSync.create();
+
+const reload = done => {
+  server.reload();
+  done();
+};
+
+const serve = done => {
+  server.init({
+    server: {
+      baseDir: './dist'
+    }
+  });
+  done();
+};
+
+const clean = () => del(['dist']);
+
+const html = () => {
+  return gulp.src(paths.html.src)
+    .pipe(gulp.dest(paths.html.dest));
+};
+
+const styles = () => {
+  return gulp.src(paths.styles.src)
+    .pipe(sass({
+      outputStyle: 'nested',
+      precision: 10,
+      includePaths: ['.'],
+      onError: console.error.bind(console, 'Sass error:')
+    }))
+    .pipe(purgeCss({
+      content: [paths.html.src]
+    }))
+    .pipe(cleanCss())
+    .pipe(gulp.dest(paths.styles.dest));
+};
+
+const images = () => {
+  return gulp.src(paths.images.src)
+    .pipe(gulp.dest(paths.images.dest));
+}
+
+const watch = () => {
+  gulp.watch([paths.html.src], gulp.series(html, reload));
+  gulp.watch([paths.styles.src], gulp.series(styles, reload));
+  gulp.watch([paths.images.src], gulp.series(images, reload));
+};
+
+gulp.task('dev', gulp.series(clean, html, styles, images, serve, watch));
+
+gulp.task('build', gulp.series(clean, html, styles, images));
